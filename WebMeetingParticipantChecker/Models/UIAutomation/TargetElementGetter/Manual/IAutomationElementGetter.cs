4ebc66bc50efc2601ebac0cd5927b9d182f51675
natsuyasai <yasai038tomato@gmail.com>
2024-09-14 09:34:52 +0900

    手動検出用のクラスを復旧

---
+++ WebMeetingParticipantChecker/Models/UIAutomation/TargetElementGetter/Manual/IAutomationElementGetter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UIAutomationClient;
+
+namespace WebMeetingParticipantChecker.Models.UIAutomation.TargetElementGetter.Manual
+{
+    internal interface IAutomationElementGetter
+    {
+        bool DetectiParticipantElement();
+        IUIAutomationElement? GetTargetElement();
+    }
+}
